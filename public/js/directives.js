586b5300378c3e53d70d9eb51b9ad85e5908a6b6
hhuai <8863824@gmail.com>
2013-04-07 22:55:05 +0800

    added validate for phone

--- public/js/directives.js
+++ public/js/directives.js
@@ -2,16 +2,28 @@
 
 /* Directives */
 
-define(['angular'],function(angular){
+define(['angular'], function (angular) {
     angular.module('myApp.directives', []).
-        directive('appVersion', ['version', function(version) {
-            return function(scope, elm, attrs) {
+        directive('appVersion', ['version', function (version) {
+            return function (scope, elm, attrs) {
                 elm.text(version);
             };
         }])
         .directive('dropdown', function () {
             return function (scope, elm, attrs) {
                 $(elm).dropdown();
+            };
+        })
+        .directive('phoneValidate', function () {
+            return {
+                require: 'ngModel',
+                link: function (scope, elm, attrs, ctrl) {
+                    ctrl.$parsers.unshift(function (viewValue) {
+                        var b = !viewValue || /^\d+$/.test(viewValue);
+                        ctrl.$setValidity('phoneformat', b);
+                        return viewValue;
+                    });
+                }
             };
         });
 });
